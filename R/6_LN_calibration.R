da8c67cf897e51855a25cf00e2c2fc7f6ad5a535
Filip Strnad <strnadf@fzp.czu.cz>
2018-03-26 11:12:01 +0200

    Update 6_LN_calibration.R

--- R/6_LN_calibration.R
+++ R/6_LN_calibration.R
@@ -2,7 +2,7 @@ source('R/1_data_import.R')
 source('R/5_LN_model.R')
 source('R/aux_fun/aux_objective_fun.R')
 
-fit.lm <- function(dta,
+fit.LN <- function(dta,
                    crit = function(x, y) 1 - R2(x, y),
                    control = DEoptim.control(itermax = 50),
                    lower = rep(1, dim(dta)[2]),
@@ -23,17 +23,22 @@ fit.lm <- function(dta,
   
 }
 
-dta.cal <- DTA[.id %in% c(2), .(Q, P, E)]
+dta.cal <- DTA[.id %in% c(2), .(Q, P, Tmax)]
 
-best.para <- fit.lm(dta.cal)
+para <- fit.LN(dta.cal, crit = function(x, y) 1 - PLC(x, y))
 
-para.history <- melt(best.para$member$bestmemit)
-
-ggplot(para.history) +
+ggplot(melt(para$member$bestmemit)) +
   geom_line(aes(x = Var1, y = value, colour = Var2)) +
-  scale_color_manual(values = c('red4','steelblue4','green4')) +
+  scale_color_manual(values = c('red4','steelblue4','green4'), labels = paste('Lag', 1:3),name = '') +
+  labs(x = 'Iteration', y = 'Value', title = 'Parameter values') +
   theme_classic()
 
-Q.cal <- LN(dta.cal, best.para$optim$bestmem)
+ggplot() +
+  geom_line(aes(x = seq_along(para$member$bestvalit), y = para$member$bestvalit), colour = 'red4') +
+  labs(x = 'Iteration', y = 'Value', title = 'Objective function values') +
+  theme_classic()
 
-model.plot(Qobs = dta.cal[, Q], Qsim = Q.cal, h = max(best.para$optim$bestmem))
+Q.cal <- LN(dta.cal, para$optim$bestmem)
+
+model.plot(Qobs = dta.cal[, Q], Qsim = Q.cal, h = max(para$optim$bestmem))
+
