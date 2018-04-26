41272eab1ac93ce88eaf00ace1657e0395d5a39b
Filip Strnad <strnadf@fzp.czu.cz>
2018-04-26 10:43:26 +0200

    Update 9_BILAN_model.R

--- R/9_BILAN_model.R
+++ R/9_BILAN_model.R
@@ -6,16 +6,22 @@ library(bilan)
 
 ?bilan
 
+dta <- DTA[.id %in% c(2),]
+dta[,T := mean(c(Tmax, Tmin)), by = DTM]
+names(dta)[which(names(dta) == 'Q')] <- 'R'
+
 bil <- bil.new('d')
-bil.set.values(bil, DTA[.id %in% c(2),])
+bil.set.values(bil, dta)
 bil.pet(bil,'latit', latitude = 50)
 bil.set.params.init(bil, list(Spa = 115, Grd = .095))
 bil.set.params.lower(bil, list(Grd = .055))
 bil.set.params.upper(bil, list(Grd = .25))
-bil.set.optim(bil, method = 'DE', crit = 'MSE', weight_BF = 0, max_iter = 500, init_GS = 50)
+bil.set.optim(bil, method = 'DE', crit = 'MSE', weight_BF = 0, max_iter = 500, init_GS = 0)
 model <- bil.optimize(bil)
 
 bil.get.values(bil)$params
+
+model.plot(model$R, model$RM, h = 0)
 
 ggplot(data = model) +
   geom_line(aes(x = DTM, y = R, colour = factor(1))) +
