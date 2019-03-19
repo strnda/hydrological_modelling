21c012f9dba467ad461af9deb595b6fce37bf798
Filip Strnad <strnadf@fzp.czu.cz>
2019-03-19 15:52:29 +0100

    Update 1_data_import.R

--- R/1_data_import.R
+++ R/1_data_import.R
@@ -1,6 +1,6 @@
 source('R/aux_fun/aux_functions.R')
 
-lop <- c('data.table', 'ggplot2', 'DEoptim')
+lop <- c('data.table', 'ggplot2', 'DEoptim', 'AMORE')
 
 to.instal <- lop[which(!lop %in% installed.packages()[,'Package'])]
 
